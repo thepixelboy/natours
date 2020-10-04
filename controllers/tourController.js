390848368deb17d5dacd91d7305abbe6bd9c2f59
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-10-04 16:47:27 +0200

    Chaining Multiple Middleware Functions

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,6 +15,16 @@ exports.checkId = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing required name or price parameters',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).send({
     status: 'success',
