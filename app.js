d122f3f5b708e235c79768bd862837920371411a
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-10-04 11:08:21 +0200

    Handling PATCH Requests

--- app.js
+++ app.js
@@ -70,6 +70,21 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updates tour here ...>',
+    },
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
