924281f100e37c6073b75ec51f027e04a08fe65e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-10-14 19:28:04 +0200

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -15,17 +15,20 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the Middleware! 👋');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// 2) ROUTES
+// 2A) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// 2B) HANDLING UNHANDLED ROUTES
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
