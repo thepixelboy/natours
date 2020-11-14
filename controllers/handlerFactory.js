3aab5611002b91009d8eb926a36b4e44bcb07a0f
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-11-14 18:10:05 +0100

    Factory Functions: Update and Create

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,6 +1,16 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const document = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: { data: document },
+    });
+  });
+
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const document = await Model.findByIdAndDelete(req.params.id);
@@ -12,5 +22,24 @@ exports.deleteOne = (Model) =>
     res.status(204).json({
       status: 'success',
       data: null,
+    });
+  });
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const document = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!document) {
+      return next(new AppError('No document found with that id', 400));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: document,
+      },
     });
   });
