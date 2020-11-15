775dd22585ed7b3818873ae32dc2adec38a309fa
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-11-15 12:41:26 +0100

    Factory Functions: Reading and solved a bug on apiFeatures.

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,21 +1,65 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const APIFeatures = require('../utils/apiFeatures');
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // To allow for nested GET reviews on tour (hack)
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+
+    // EXECUTE QUERY
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const doc = await features.query;
+
+    // SEND RESPONSE
+    res.status(200).send({
+      status: 'success',
+      results: doc.length,
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('No document found with that id', 400));
+    }
+
+    res.status(200).send({
+      status: 'success',
+      data: {
+        doc,
+      },
+    });
+  });
 
 exports.createOne = (Model) =>
   catchAsync(async (req, res, next) => {
-    const document = await Model.create(req.body);
+    const doc = await Model.create(req.body);
 
     res.status(201).json({
       status: 'success',
-      data: { data: document },
+      data: { data: doc },
     });
   });
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
-    const document = await Model.findByIdAndDelete(req.params.id);
+    const doc = await Model.findByIdAndDelete(req.params.id);
 
-    if (!document) {
+    if (!doc) {
       return next(new AppError('No document found with that id', 400));
     }
 
@@ -27,19 +71,19 @@ exports.deleteOne = (Model) =>
 
 exports.updateOne = (Model) =>
   catchAsync(async (req, res, next) => {
-    const document = await Model.findByIdAndUpdate(req.params.id, req.body, {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true,
     });
 
-    if (!document) {
+    if (!doc) {
       return next(new AppError('No document found with that id', 400));
     }
 
     res.status(200).json({
       status: 'success',
       data: {
-        data: document,
+        data: doc,
       },
     });
   });
--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,21 +1,6 @@
 const Review = require('../models/reviewModel');
-const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
-
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-
-  const reviews = await Review.find(filter);
-
-  res.status(200).send({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews,
-    },
-  });
-});
+// const catchAsync = require('../utils/catchAsync');
 
 exports.setTourAndUserIds = (req, res, next) => {
   // Allow nested routes
@@ -24,6 +9,8 @@ exports.setTourAndUserIds = (req, res, next) => {
   next();
 };
 
+exports.getAllReviews = factory.getAll(Review);
+exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
