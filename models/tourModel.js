6bb6acf8456ca5b0e2bb17dfcf41c6bbf7fd3096
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-11-17 19:29:33 +0100

    Geospatial Aggregation: Calculating Distances

--- models/tourModel.js
+++ models/tourModel.js
@@ -175,11 +175,11 @@ tourSchema.pre(/^find/, function (next) {
 // });
 
 // AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  // console.log(this.pipeline());
-  next();
-});
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+//   console.log(this.pipeline());
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
