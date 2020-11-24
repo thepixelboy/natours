5d1633ae66852373b5b88c1af9d2aa8cc5d60b2e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-11-24 18:34:36 +0100

    Logging out Users

--- public/js/login.js
+++ public/js/login.js
@@ -23,3 +23,16 @@ export const login = async (email, password) => {
     showAlert('error', err.response.data.message);
   }
 };
+
+export const logout = async () => {
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://localhost:3000/api/v1/users/logout',
+    });
+    if (res.data.status === 'success') location.reload();
+  } catch (err) {
+    console.log(err.response);
+    showAlert('error', 'Error loggin out! Try again.');
+  }
+};
